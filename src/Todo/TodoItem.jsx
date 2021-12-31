eae816cc6593281d1784c06b826482df9ac240e5
Harsh Mehta <harshmehta813@gmail.com>
2021-12-31 16:33:58 +0530

    Initial commit

---
+++ src/Todo/TodoItem.jsx
@@ -0,0 +1,40 @@
+import React from "react";
+
+const delay = (time) => {
+  const start = Date.now();
+  while (Date.now() - start < time) {
+    continue;
+  }
+  return start;
+};
+
+const areEqual = (prevProps, nextProps) => {
+  if (prevProps.status === nextProps.status) {
+    return true;
+  }
+  return false;
+};
+
+const TodoItem = ({ title, id, status, handleDelete, handleToggle }) => {
+  /** Expensive calculation */
+  const time = delay(200);
+  return (
+    <div
+      style={{
+        display: "flex",
+        justifyContent: "center",
+        alignItems: "center",
+        gap: "1rem"
+      }}
+    >
+      <h3>{title}</h3>
+      <div>{time}</div>
+      <h3>{`${status ? "DONE" : "PENDING"}`}</h3>
+      <button onClick={() => handleToggle(id)}>TOGGLE</button>
+    </div>
+  );
+};
+
+export const MemoisedTodoItem = React.memo(TodoItem, areEqual);
+
+export default TodoItem;
